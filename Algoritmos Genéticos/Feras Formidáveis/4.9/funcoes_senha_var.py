4fddd5acc1ff4279b600f885700744c71fd6a961
Andriel da Silva <andrielm02@gmail.com>
2025-06-12 18:57:27 -0300

    Add files via upload

---
+++ Algoritmos Genéticos/Feras Formidáveis/4.9/funcoes_senha_var.py
@@ -0,0 +1,344 @@
+import random
+
+###############################################################################
+#                                    Senha                                    #
+###############################################################################
+
+
+def gene_senha(letras_possiveis):
+    """Sorteia uma letra.
+
+    Args:
+      letras: letras possíveis de serem sorteadas.
+
+    """
+    letra = random.choice(letras_possiveis)
+    return letra
+
+
+def cria_candidato_senha(letras_possiveis): #foi atualizado para não depender do tamanho da senha
+    """Cria um candidato para o problema da senha
+
+    Args:
+      tamanho_senha: inteiro representando o tamanho da senha.
+      letras_possiveis: letras possíveis de serem sorteadas.
+
+    """
+    candidato = []
+    indice_tamanho = random.randint(1, 30)
+
+    for _ in range(indice_tamanho):
+        candidato.append(gene_senha(letras_possiveis))
+
+    return candidato
+
+
+def populacao_senha(tamanho_populacao, letras_possiveis):
+    """Cria população inicial no problema da senha
+
+    Args
+      tamanho_populacao: tamanho da população.
+      tamanho_senha: inteiro representando o tamanho da senha.
+      letras_possiveis: letras possíveis de serem sorteadas.
+
+    """
+    populacao = []
+
+    for _ in range(tamanho_populacao):
+        populacao.append(cria_candidato_senha(letras_possiveis))
+
+    return populacao
+
+
+def funcao_objetivo_senha(candidato, senha_verdadeira): #sofreu modificações para não precisar do tamanho da senha
+    """Computa a funcao objetivo de um candidato no problema da senha
+
+    Args:
+      candidato: um palpite para a senha que você quer descobrir
+      senha_verdadeira: a senha que você está tentando descobrir
+
+    """
+    distancia = 0
+
+    if len(candidato) == len(senha_verdadeira): #se o tamanho da senha candidata for verdadeiro
+        for letra_candidato, letra_senha in zip(candidato, senha_verdadeira):
+            num_letra_candidato = ord(letra_candidato)
+            num_letra_senha = ord(letra_senha)
+            distancia += abs(num_letra_candidato - num_letra_senha)
+
+        return distancia    
+    else:
+        return float("inf")
+
+def funcao_objetivo_pop_senha(populacao, senha_verdadeira):
+    """Computa a funcao objetivo de uma populaçao no problema da senha.
+
+    Args:
+      populacao: lista contendo os individuos do problema
+      senha_verdadeira: a senha que você está tentando descobrir
+
+    """
+    fitness = []
+
+    for individuo in populacao:
+        fitness.append(funcao_objetivo_senha(individuo, senha_verdadeira))
+
+    return fitness
+
+
+###############################################################################
+#                                   Seleção                                   #
+###############################################################################
+
+
+def selecao_roleta_max(populacao, fitness):
+    """Realiza seleção da população pela roleta
+
+    Args:
+      populacao: lista contendo os individuos do problema
+      fitness: lista contendo os valores computados da funcao objetivo
+
+    """
+    selecionados = random.choices(populacao, fitness, k=len(populacao))
+    return selecionados
+
+
+def selecao_torneio_max(populacao, fitness, tamanho_torneio):
+    """Faz a seleção de uma população usando torneio.
+
+    Nota: da forma que está implementada, só funciona em problemas de
+    maximização.
+
+    Args:
+      populacao: lista contendo os individuos do problema
+      fitness: lista contendo os valores computados da funcao objetivo
+      tamanho_torneio: quantidade de invíduos que batalham entre si
+
+    """
+    selecionados = []
+
+    for _ in range(len(populacao)):
+        sorteados = random.sample(populacao, tamanho_torneio)
+
+        fitness_sorteados = []
+        for individuo in sorteados:
+            indice_individuo = populacao.index(individuo)
+            fitness_sorteados.append(fitness[indice_individuo])
+
+        max_fitness = max(fitness_sorteados)
+        indice_max_fitness = fitness_sorteados.index(max_fitness)
+        individuo_selecionado = sorteados[indice_max_fitness]
+
+        selecionados.append(individuo_selecionado)
+
+    return selecionados
+
+
+def selecao_torneio_min(populacao, fitness, tamanho_torneio):
+    """Faz a seleção de uma população usando torneio.
+
+    Nota: da forma que está implementada, só funciona em problemas de
+    minimização.
+
+    Args:
+      populacao: lista contendo os individuos do problema
+      fitness: lista contendo os valores computados da funcao objetivo
+      tamanho_torneio: quantidade de invíduos que batalham entre si
+
+    """
+    selecionados = []
+
+    for _ in range(len(populacao)):
+        sorteados = random.sample(populacao, tamanho_torneio)
+
+        fitness_sorteados = []
+        for individuo in sorteados:
+            indice_individuo = populacao.index(individuo)
+            fitness_sorteados.append(fitness[indice_individuo])
+
+        min_fitness = min(fitness_sorteados)
+        indice_min_fitness = fitness_sorteados.index(min_fitness)
+        individuo_selecionado = sorteados[indice_min_fitness]
+
+        selecionados.append(individuo_selecionado)
+
+    return selecionados
+
+
+###############################################################################
+#                                  Cruzamento                                 #
+###############################################################################
+
+
+def cruzamento_uniforme(pai, mae, chance_de_cruzamento):
+    """Realiza cruzamento uniforme
+
+    Args:
+      pai: lista representando um individuo
+      mae: lista representando um individuo
+      chance_de_cruzamento: float entre 0 e 1 representando a chance de cruzamento
+
+    """
+    if random.random() < chance_de_cruzamento:
+        filho1 = []
+        filho2 = []
+
+        for gene_pai, gene_mae in zip(pai, mae):
+            if random.choice([True, False]):
+                filho1.append(gene_pai)
+                filho2.append(gene_mae)
+            else:
+                filho1.append(gene_mae)
+                filho2.append(gene_pai)
+
+        return filho1, filho2
+    else:
+        return pai, mae
+
+
+def cruzamento_ponto_duplo(pai, mae, chance_de_cruzamento):
+    """Realiza cruzamento de ponto duplo
+
+    Args:
+      pai: lista representando um individuo
+      mae: lista representando um individuo
+      chance_de_cruzamento: float entre 0 e 1 representando a chance de cruzamento
+
+    """
+    if random.random() < chance_de_cruzamento:
+        corte1 = random.randint(1, len(mae) - 2)
+        corte2 = random.randint(corte1 + 1, len(mae) - 1)
+        filho1 = pai[:corte1] + mae[corte1:corte2] + pai[corte2:]
+        filho2 = mae[:corte1] + pai[corte1:corte2] + mae[corte2:]
+        return filho1, filho2
+    else:
+        return pai, mae
+
+
+def cruzamento_ponto_simples(pai, mae, chance_de_cruzamento):
+    """Realiza cruzamento de ponto simples
+
+    Args:
+      pai: lista representando um individuo
+      mae: lista representando um individuo
+      chance_de_cruzamento: float entre 0 e 1 representando a chance de cruzamento
+
+    """
+    if random.random() < chance_de_cruzamento:
+        corte = random.randint(1, len(mae) - 1)
+        filho1 = pai[:corte] + mae[corte:]
+        filho2 = mae[:corte] + pai[corte:]
+        return filho1, filho2
+    else:
+        return pai, mae
+
+
+###############################################################################
+#                                   Mutação                                   #
+###############################################################################
+
+
+def mutacao_salto(populacao, chance_de_mutacao, valores_possiveis):
+    """Realiza mutação de salto
+
+    Args:
+      populacao: lista contendo os indivíduos do problema
+      chance_de_mutacao: float entre 0 e 1 representando a chance de mutação
+      valores_possiveis: lista com todos os valores possíveis dos genes
+
+    """
+    for individuo in populacao:
+        if random.random() < chance_de_mutacao:
+            gene = random.randint(0, len(individuo) - 1)
+            valor_gene = individuo[gene]
+            indice_letra = valores_possiveis.index(valor_gene)
+            indice_letra += random.choice([1, -1])
+            indice_letra %= len(valores_possiveis)
+            individuo[gene] = valores_possiveis[indice_letra]
+
+
+def mutacao_simples(populacao, chance_de_mutacao, valores_possiveis):
+    """Realiza mutação simples
+
+    Args:
+      populacao: lista contendo os indivíduos do problema
+      chance_de_mutacao: float entre 0 e 1 representando a chance de mutação
+      valores_possiveis: lista com todos os valores possíveis dos genes
+
+    """
+    for individuo in populacao:
+        if random.random() < chance_de_mutacao:
+            gene = random.randint(0, len(individuo) - 1)
+            valor_gene = individuo[gene]
+            valores_sorteio = set(valores_possiveis) - set([valor_gene])
+            individuo[gene] = random.choice(list(valores_sorteio))
+
+
+def mutacao_simples_cb(populacao, chance_de_mutacao):
+    """Realiza mutação simples no problema das caixas binárias
+
+    Args:
+      populacao: lista contendo os indivíduos do problema
+      chance_de_mutacao: float entre 0 e 1 representando a chance de mutação
+
+    """
+    for individuo in populacao:
+        if random.random() < chance_de_mutacao:
+            gene = random.randint(0, len(individuo) - 1)
+            individuo[gene] = 0 if individuo[gene] == 1 else 1
+
+
+def mutacao_sucessiva_cb(populacao, chance_de_mutacao, chance_mutacao_gene):
+    """Realiza mutação simples no problema das caixas binárias
+
+    Args:
+      populacao: lista contendo os indivíduos do problema
+      chance_de_mutacao: float entre 0 e 1 representando a chance de mutação
+      chance_mutacao_gene: float entre 0 e 1 representando a chance de mutação de cada gene
+
+    """
+    for individuo in populacao:
+        if random.random() < chance_de_mutacao:
+            for gene in range(len(individuo)):
+                if random.random() < chance_mutacao_gene:
+                    individuo[gene] = 0 if individuo[gene] == 1 else 1
+
+
+def mutacao_simples_cnb(populacao, chance_de_mutacao, valor_max):
+    """Realiza mutação simples no problema das caixas não-binárias
+
+    Args:
+      populacao: lista contendo os indivíduos do problema
+      chance_de_mutacao: float entre 0 e 1 representando a chance de mutação
+      valor_max: inteiro represtando o valor máximo das caixas
+
+    """
+    for individuo in populacao:
+        if random.random() < chance_de_mutacao:
+            gene = random.randint(0, len(individuo) - 1)
+            valor_gene = individuo[gene]
+            valores_possiveis = list(range(valor_max + 1))
+            valores_possiveis.remove(valor_gene)
+            individuo[gene] = random.choice(valores_possiveis)
+
+
+def mutacao_sucessiva_cnb(
+    populacao, chance_de_mutacao, chance_mutacao_gene, valor_max
+):
+    """Realiza mutação simples no problema das caixas não-binárias
+
+    Args:
+      populacao: lista contendo os indivíduos do problema
+      chance_de_mutacao: float entre 0 e 1 representando a chance de mutação
+      chance_mutacao_gene: float entre 0 e 1 representando a chance de mutação de cada gene
+      valor_max: inteiro represtando o valor máximo das caixas
+
+    """
+    for individuo in populacao:
+        if random.random() < chance_de_mutacao:
+            for gene in range(len(individuo)):
+                if random.random() < chance_mutacao_gene:
+                    valores_possiveis = list(range(valor_max + 1))
+                    valor_gene = individuo[gene]
+                    valores_possiveis.remove(valor_gene)
+                    individuo[gene] = random.choice(valores_possiveis)
